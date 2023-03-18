a3b288adcff4e5fd4f81952bd5585b90cf5bb5f3
stijns96 <47820758+stijns96@users.noreply.github.com>
2023-03-18 23:29:03 +0100

    Conditionally display snippets based on liquid tag

--- extension.js
+++ extension.js
@@ -1,92 +1,130 @@
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
-const recommendedExtensions = [
-  {
-    id: 'shopify.theme-check-vscode',
-    name: 'Shopify Liquid'
-  }
-];
+let allSnippets;
 
-const notRecommendedExtensions = [
-  {
-    id: 'sissel.shopify-liquid',
-    name: 'Liquid'
-  },
-  {
-    id: 'neilding.language-liquid',
-    name: 'Liquid Languages Support'
-  },
-  {
-    id: 'killalau.vscode-liquid-snippets',
-    name: 'Shopify Liquid Template Snippets'
-  }
-]
+function getSnippets(snippetsPath) {
+  const snippetsContent = fs.readFileSync(snippetsPath, 'utf-8');
+  return JSON.parse(snippetsContent);
+}
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
-  let message = 'The following extensions are recommended for this extension:\n\n';
-
-  recommendedExtensions.forEach(({id, name}) => {
-    const extension = vscode.extensions.getExtension(id);
-
-    if (!extension) {
-      message += `- ${name}\n`;
-    }
-  });
-
-  if (message !== 'The following extensions are recommended for this extension:\n\n') {
-    message += 'Do you want to install them now?';
-    vscode.window.showInformationMessage(message, {modal: true}, 'Yes', 'No')
-    .then(install => {
-      if (install === 'Yes') {
-        recommendedExtensions.forEach(({id}) => {
-          vscode.commands.executeCommand('workbench.extensions.installExtension', id);
-        });
-      }
-      vscode.window.showInformationMessage('Extensions installed successfully. Please reload the editor for changes to take effect.', 'Reload')
-      .then(reload => {
-        if (reload === 'Reload') {
-          vscode.commands.executeCommand('workbench.action.reloadWindow');
+  const disposable = vscode.languages.registerCompletionItemProvider(
+    'liquid', // language ID for Liquid files
+    {
+      provideCompletionItems(document, position) {
+        // Get the entire text of the document
+        const text = document.getText();
+        
+        // Check if the cursor is inside the {%- liquid ... -%} tags using a regex
+        const regex = /\{%-\s*liquid([\s\S]*?)-%\}/g;
+        
+        let match;
+        const completionItems = [];
+        
+        while ((match = regex.exec(text)) !== null) {
+          
+          const conditionalSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'liquid', 'conditional.code-snippets');
+          const conditionalSnippets = getSnippets(conditionalSnippetsPath);
+          
+          const iterationSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'liquid', 'iteration.code-snippets');
+          const iterationSnippets = getSnippets(iterationSnippetsPath);
+          
+          const syntaxSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'liquid', 'syntax.code-snippets');
+          const syntaxSnippets = getSnippets(syntaxSnippetsPath);
+          
+          const themeSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'liquid', 'theme.code-snippets');
+          const themeSnippets = getSnippets(themeSnippetsPath);
+          
+          const variableSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'liquid', 'variable.code-snippets');
+          const variableSnippets = getSnippets(variableSnippetsPath);
+        
+          allSnippets = {
+            ...conditionalSnippets,
+            ...iterationSnippets,
+            ...syntaxSnippets,
+            ...themeSnippets,
+            ...variableSnippets
+          };
+          
+          const start = match.index + match[0].indexOf('liquid') + 7;
+          const end = match.index + match[0].lastIndexOf('-%}') + 1;
+          
+          // Check if the cursor is within the Liquid tags
+          if (position.isAfter(new vscode.Position(document.positionAt(start).line, document.positionAt(start).character))
+          && position.isBefore(new vscode.Position(document.positionAt(end).line, document.positionAt(end).character))) {
+            
+            for (const key in allSnippets) {
+              if (Object.hasOwnProperty.call(allSnippets, key)) {
+                const snippet = allSnippets[key];
+                
+                const completionItem = new vscode.CompletionItem(snippet.prefix, vscode.CompletionItemKind.Snippet);
+                if (Array.isArray(snippet.body)) {
+                  completionItem.insertText = new vscode.SnippetString(snippet.body.join('\n'));
+                } else {
+                  completionItem.insertText = new vscode.SnippetString(snippet.body);
+                }
+                completionItem.documentation = snippet.description;
+                
+                completionItems.push(completionItem);
+              }
+            }
+            
+            // If the cursor is inside the {%- liquid -%} tags, return tags without syntax
+            return completionItems;
+          }
         }
-      });
-    });
-  }
-
-  message = 'The following extensions are not recommended for this extension:\n\n';
-
-  notRecommendedExtensions.forEach(({id, name}) => {
-    const extension = vscode.extensions.getExtension(id);
-
-    if (extension) {
-      message += `- ${name}\n`;
-    }
-  });
-
-  if (message !== 'The following extensions are not recommended for this extension:\n\n') {
-    message += 'Do you want to uninstall them now?';
-    vscode.window.showWarningMessage(message, {modal: true}, 'Yes', 'No')
-    .then(uninstall => {
-      if (uninstall === 'Yes') {
-        notRecommendedExtensions.forEach(({id}) => {
-          vscode.commands.executeCommand('workbench.extensions.uninstallExtension', id);
-        });
-      }
-      vscode.window.showInformationMessage('Extensions uninstalled successfully. Please reload the editor for changes to take effect.', 'Reload')
-      .then(reload => {
-        if (reload === 'Reload') {
-          vscode.commands.executeCommand('workbench.action.reloadWindow');
+        
+        const conditionalSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'conditional.code-snippets');
+        const conditionalSnippets = getSnippets(conditionalSnippetsPath);
+        
+        const iterationSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'iteration.code-snippets');
+        const iterationSnippets = getSnippets(iterationSnippetsPath);
+        
+        const syntaxSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'syntax.code-snippets');
+        const syntaxSnippets = getSnippets(syntaxSnippetsPath);
+        
+        const themeSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'theme.code-snippets');
+        const themeSnippets = getSnippets(themeSnippetsPath);
+        
+        const variableSnippetsPath = path.join(context.extensionPath, 'snippets', 'tags', 'variable.code-snippets');
+        const variableSnippets = getSnippets(variableSnippetsPath);
+        
+        allSnippets = {
+          ...conditionalSnippets,
+          ...iterationSnippets,
+          ...syntaxSnippets,
+          ...themeSnippets,
+          ...variableSnippets
+        };
+        
+        for (const key in allSnippets) {
+          if (Object.hasOwnProperty.call(allSnippets, key)) {
+            const snippet = allSnippets[key];
+            
+            const completionItem = new vscode.CompletionItem(snippet.prefix, vscode.CompletionItemKind.Snippet);
+            if (Array.isArray(snippet.body)) {
+              completionItem.insertText = new vscode.SnippetString(snippet.body.join('\n'));
+            } else {
+              completionItem.insertText = new vscode.SnippetString(snippet.body);
+            }
+            completionItem.documentation = snippet.description;
+            
+            completionItems.push(completionItem);
+          }
         }
-      });
-    });
+        
+        // If the cursor is not inside the {%- liquid -%} tags, return tags with syntax
+        return completionItems;
+      }
+    }
+    );
+    
+    context.subscriptions.push(disposable);
   }
-}
-
-// This method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
+  
+  module.exports = {
+    activate
+  };
+  
