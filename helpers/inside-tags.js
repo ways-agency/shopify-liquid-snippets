c356dfd50db4f5d524b1e6c9eaf64b07160f7f3c
Stijn <stijn.slats@gmail.com>
2023-09-17 19:18:31 +0200

    Create a better check for inside tags

--- helpers/inside-tags.js
+++ helpers/inside-tags.js
@@ -1,30 +1,14 @@
-const vscode = require("vscode");
-
 function insideTags(document, position, regex) {
   const text = document.getText();
 
   let match;
 
   while ((match = regex.exec(text)) !== null) {
-    // Get the start and end positions of the {%- liquid -%} tags
-    const start = match.index + match[0].indexOf("liquid") + 7;
-    const end = match.index + match[0].lastIndexOf("-%}") + 1;
+    const tagStart = document.positionAt(match.index);
+    const tagEnd = document.positionAt(match.index + match[0].length);
 
     // Check if the cursor is within the Liquid tags
-    if (
-      position.isAfter(
-        new vscode.Position(
-          document.positionAt(start).line,
-          document.positionAt(start).character
-        )
-      ) &&
-      position.isBefore(
-        new vscode.Position(
-          document.positionAt(end).line,
-          document.positionAt(end).character
-        )
-      )
-    ) {
+    if (position.isAfterOrEqual(tagStart) && position.isBeforeOrEqual(tagEnd)) {
       return true;
     }
   }
